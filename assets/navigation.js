d71d24eb5b579ac46f880b6e0d8179bbbd67e505
digimbyte <digimbyte@users.noreply.github.com>
2024-06-23 04:18:47 +0000

    deploy: 7af55df37c76fc9c4d6c15363a88ad813a022e18

--- assets/navigation.js
+++ assets/navigation.js
@@ -2,7 +2,7 @@
 // when the user clicks a link, we update the location
 // redirect.js will handle the content
 function goto(dest,stub=false) {
-    const loc = dest.startsWith('pages/') ? dest.slice(6) : dest
+    const loc = dest.startsWith('pages/') ? dest.match(/pages\/(.+)\.md/)[1]: dest
     // this triggers redirect to load the page content
     console.log("goto", loc, "dest", dest)
     if(stub){
